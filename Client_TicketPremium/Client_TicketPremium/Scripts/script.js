43903734b129ffbe477fbc13ad95595cdf621455
Christopher <csrm1997@hotmail.com>
2019-06-11 20:45:36 -0500

    Index and soccer game interface
    Created the client project with index and soccer game interface

---
+++ Client_TicketPremium/Client_TicketPremium/Scripts/script.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('[data-toggle=tooltip]').tooltip();
+});
+function cambiar(partido, cantidades, index, localidad) {
+    var url = "http://localhost:8080/TicketPremium/FacturaServlet?";
+    for (var i = 0; i < cantidades.length; i++) {
+        if (i === index) {
+            var cant = "cantidad" + i;
+            url += "cantidad" + i + "=" + document.getElementById(cant).value + "&";
+        } else {
+            url += "cantidad" + i + "=" + cantidades[i] + "&";
+        }
+    }
+    url += "partido=" + partido + "&localidad=" + localidad;
+    location.replace(url);
+}
