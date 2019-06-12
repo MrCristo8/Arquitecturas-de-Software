75e444cfcc3b1410b277ac5ddb9816cdd7363695
Alejandro <jona-alejo14@hotmail.com>
2019-06-11 21:02:11 -0500

    Create models
    Models of: Bill, SoccerGame, Client and Locality

---
+++ Client_TicketPremium/Client_TicketPremium/Models/Bill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client_TicketPremiun.Models
+{
+    public class Bill
+    {
+        public string code_client { get; set; }
+        public string name_client { get; set; }
+        public int code_bill { get; set; }
+        public string date_bill { get; set; }
+        public string way_pay { get; set; }
+        public SoccerGame soccer_game { get; set; }
+    }
+}
---
+++ Client_TicketPremium/Client_TicketPremium/Models/SoccerGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client_TicketPremiun.Models
+{
+    public class SoccerGame
+    {
+        public string code_soccergame { get; set; }
+        public string local_team { get; set; }
+        public string visitor_team { get; set; }
+        public string place { get; set; }
+        public string date_soccergame { get; set; }
+        public List<Locality> localities { get; set; }
+
+    }
+}
