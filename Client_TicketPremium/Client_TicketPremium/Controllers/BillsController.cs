1d138344c2d64fce1abdbcec33b9ae896f4f2a3c
Christopher <csrm1997@hotmail.com>
2019-06-13 12:55:47 -0500

    Some changes

--- Client_TicketPremium/Client_TicketPremium/Controllers/BillsController.cs
+++ Client_TicketPremium/Client_TicketPremium/Controllers/BillsController.cs
@@ -10,14 +10,15 @@ namespace Client_TicketPremium.Controllers
     public class BillsController : Controller
     {
         // GET: Bills
-        public ActionResult Buy(string codeSoccerGame)
+        public ActionResult Buy()
         {
+            string codeSoccerGame = Request["code"];
             using (WSTicket.WSTicketClient client = new WSTicket.WSTicketClient())
             {
                 SoccerGame soccerGame = client.getSoccerGame(codeSoccerGame);
+                ViewBag.clients = client.getClients().ToList();
                 return View(soccerGame);
             }
-            return View();
         }
     }
 }
