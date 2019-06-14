ec0f580bb729de8c6216f94f402ccad2ef68b83c
Alejandro <jona-alejo14@hotmail.com>
2019-06-13 20:38:11 -0500

    Implementation Reports

---
+++ Client_TicketPremium/Client_TicketPremium/Controllers/ReportController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Client_TicketPremium.WSTicket;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client_TicketPremium.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SoccerGamesAvailable()
+        {
+            using (WSTicket.WSTicketClient client = new WSTicket.WSTicketClient())
+            {
+                List<SoccerGame> soccerGamesAvailable = client.getAvailableSoccerGames().ToList();
+                return View(soccerGamesAvailable);
+            }
+        }
+
+        public ActionResult SoccerGamesNotAvailable()
+        {
+            using (WSTicket.WSTicketClient client = new WSTicket.WSTicketClient())
+            {
+                List<SoccerGame> soccerGamesNotAvailable = client.getNotAvailableSoccerGames().ToList();
+                return View(soccerGamesNotAvailable);
+            }
+        }
+
+        public ActionResult GetReport()
+        {
+            string codeSoccerGame = Request["code"];
+            using (WSTicket.WSTicketClient client = new WSTicket.WSTicketClient())
+            {
+                SoccerGame soccerGame = client.getReportSoccerGame(codeSoccerGame);
+                ViewBag.code = soccerGame.code_soccergame;
+                return View(soccerGame);
+            }
+        }
+    }
+}
